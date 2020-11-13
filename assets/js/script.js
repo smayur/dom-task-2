b530667a2acfea2b6aaa375742c6012a949e4602
mayurS <mayurs@prdxn.com>
2020-11-13 18:51:47 +0530

    add script for toggle menu

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var menu = document.querySelector('.primary-navbar');
+var burger = document.querySelector('.burger');
+
+burger.addEventListener('click', function(){ 
+  //Toggle nav
+  burger.classList.toggle('navtoggle');
+  menu.classList.toggle("show-menu");
+  menu.classList.toggle("hide-menu");
+});
