02ea999a9f82f66e221e7524d220d2fdd44c1653
mayurS <mayurs@prdxn.com>
2020-11-13 19:55:23 +0530

    remove show-menu  toggle script

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,5 @@ var burger = document.querySelector('.burger');
 burger.addEventListener('click', function(){ 
   //Toggle nav
   burger.classList.toggle('navtoggle');
-  menu.classList.toggle("show-menu");
-  menu.classList.toggle("hide-menu");
+  menu.classList.toggle('hide-menu');
 });
