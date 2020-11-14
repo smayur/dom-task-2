c0c39e662e16f0337391451ffce92b92fe22098a
mayurS <mayurs@prdxn.com>
2020-11-14 11:57:13 +0530

    add clear form script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var regexForText=/^[a-z A-Z]+$/;
-var regexForEmail=/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/;
+var regexForEmail=/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 var menu = document.querySelector('.primary-navbar');
 var burger = document.querySelector('.burger');
+var form = document.querySelectorAll('form');
 var firstName = document.querySelector('.firstname');
 var lastName = document.querySelector('.lastname');
 var position = document.querySelector('.position');
@@ -11,7 +12,6 @@ var countries = document.querySelector('.countries');
 var workEmail = document.querySelector('.work-email');
 var checkBox = document.querySelectorAll('input[type="checkbox"]');
 var submit = document.querySelectorAll('.submit');
-var flag = true;
 
 burger.addEventListener('click', function(){ 
   //Toggle nav
@@ -21,26 +21,23 @@ burger.addEventListener('click', function(){
 
 
 submit[1].addEventListener('click', function(e){
-  e.preventDefault();
-  flag = checkfields();
-  if (flag) {
-    addToTable();
-  }
+  e.preventDefault(); 
+  checkfields();
 });
 
 
 function checkfields() {
-  if (firstName.value == null || firstName.value == '') { // check first name is not empty
+  if (firstName.value == null || firstName.value == '') { //check first name is not empty
     alert("Please Enter Your First Name"); 
     flag = false;
   }else if (!firstName.value.match(regexForText)) { //check letters in first name
     alert("Please Use Letters In First Name.");
-  }else if (lastName.value == null || lastName.value == '') { // check last name is not empty
+  }else if (lastName.value == null || lastName.value == '') { //check last name is not empty
     flag = false;
     alert("Please Enter Your Last Name");
   }else if (!lastName.value.match(regexForText)) { //check letters in last name
     alert("Please Use Letters In Last Name.");
-  }else if (position.value == null || position.value == '') { // check position name is not empty
+  }else if (position.value == null || position.value == '') { //check position name is not empty
     flag = false;
     alert("Please Enter Your Position");
   }else if (!position.value.match(regexForText)) { //check letters in position
@@ -48,21 +45,29 @@ function checkfields() {
   }else if (company.value == null || company.value == '') {
     flag = false;
     alert("Please Enter Your Company Name");
-  }else if (companyType.value === "none") {
+  }else if (companyType.value === "none") { //check company type is selected
     flag = false;
     alert("Please Select Your Company Type");
-  }else if (countries.value === "none") {
+  }else if (countries.value === "none") { //check country is selected
     flag = false;
     alert("Please Select Your Country");
-  }else if (workEmail.value == null || workEmail.value == '') {
+  }else if (workEmail.value == null || workEmail.value == '') { //check email input is not empty
     flag = false;
     alert("Please Enter Your Email Address");
-  }else if (!workEmail.value.match(regexForEmail)) { //check letters in position
+  }else if (!workEmail.value.match(regexForEmail)) { //check email input with regular expression
     alert("Please match the format requested.");
-  }else if (checkBox[0].checked === false && checkBox[1].checked === false) {
+  }else if (checkBox[0].checked === false && checkBox[1].checked === false) { //if both checkbox empty
+    flag = false;
+    alert("Please Select Either Yes or No");
+  }else if (checkBox[0].checked === true && checkBox[1].checked === true) { //if both checkbox checked
     flag = false;
     alert("Please Select Either Yes or No");
   }else { 
-    return flag; 
-  }
+    alert("Thanks To Subscribe Our Newsletter!"); 
+    clearForm();
+  } 
+}
+
+function clearForm() {
+  form[1].reset(); //reset the form
 }
